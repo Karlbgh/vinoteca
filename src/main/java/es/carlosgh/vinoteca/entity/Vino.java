7c771a0956dee7c6b75e2e8424ed9110dfb51bba
carlos gonzalez <carlos22gh@gmail.com>
2022-10-30 20:11:21 +0100

    Se añade validaciones a la clase Vino

--- src/main/java/es/carlosgh/vinoteca/entity/Vino.java
+++ src/main/java/es/carlosgh/vinoteca/entity/Vino.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -17,17 +19,23 @@ import java.time.LocalDate;
 @Builder
 public class Vino {
     private Long id;
+
     @NotNull(message="{vino.tipo.vacio}")
     private TipoVino tipo;
-    @NotEmpty(message="{vino.tipo.vacio}")
-    private String origen;
-    @NotEmpty(message="{vino.tipo.vacio}")
+
+    @NotNull(message="{vino.precio.vacio}")
+    private Double precio;
+
+    @NotEmpty(message="{vino.graduacion.vacio}")
     private String graduacion;
-    @NotEmpty(message="{vino.tipo.vacio}")
+
+    @NotEmpty(message="{vino.year.vacio}")
     private String year;
-    @NotEmpty(message="{vino.tipo.vacio}")
+
     private String imagen;
-    @NotEmpty(message="{vino.tipo.vacio}")
+
+    @NotEmpty(message="{vino.desc.vacio}")
+    @Length( max = 255, min = 20 ,message = "{vino.desc.max}")
     private String descripcion;
 
     public Vino(Long id) {
